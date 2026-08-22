1fbea1c9320dc4ca6630c937efbfcc18bb4682ae
Faiyaz Mahmud <faiyazmahmud2002@gmail.com>
2026-08-22 23:41:12 +0600

    users table added

--- src/main/java/com/tasked/modular/ModularApplication.java
+++ src/main/java/com/tasked/modular/ModularApplication.java
@@ -2,8 +2,10 @@ package com.tasked.modular;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ModularApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/tasked/modular/shared/enums/Role.java
@@ -0,0 +1,6 @@
+package com.tasked.modular.shared.enums;
+
+public enum Role {
+    User,
+    Admin
+}
